204cd0ad1fb8c72c78a0bbb85d604234a7431015
Гость <Guest@iMac-Mac005-36.local>
2022-04-22 10:10:20 +0900

    first commit

---
+++ MFP calc/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MFP calc
+//
+//  Created by Гость on 16.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var buttonone: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        buttonone.layer.cornerRadius = 34
+        button2.layer.cornerRadius = 34
+        
+        
+    }
+
+
+}
+
